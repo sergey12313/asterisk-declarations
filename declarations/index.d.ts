8e6061fcd232637251e0e8ac620fcbaed0bb22b3
Sergey Razuvaev <sergey12313@gmail.com>
2021-10-13 09:34:20 +0300

    add base def

---
+++ declarations/index.d.ts
@@ -0,0 +1,33 @@
+type CallerIdDataTypes = "name" | "num" | "all" | "ANI" | "DNIS" | "RDNIS";
+
+interface CalleridMethods {
+  get: () => string;
+  set: (str: string) => void;
+}
+
+/** @noSelf **/
+declare namespace app {
+  export function dial(str: string): void;
+  export function playback(str: string): void;
+  export const hangup: () => void;
+  export const set: (str: string) => void;
+  export const answer: () => void;
+  export const musiconhold: (str?: string) => void;
+  export const transfer: (str: string) => void;
+  export const playtones: (str: string) => void;
+  export const wait: (time: number) => void;
+  export const noop: (str: string) => void;
+  export const MP3Player: (str: string) => void;
+  export const queue: (str: string) => void;
+}
+
+type ExtensionType = {
+  [key: string]: { [key: string]: (context: string, exten: string) => void };
+};
+
+/** @noSelf **/
+declare namespace channel {
+  export const CALLERID: (type: CallerIdDataTypes) => CalleridMethods;
+  export const EXTEN: { get: () => string };
+  export const PJSIP_HEADER: (a: string, b: string) => CalleridMethods;
+}
